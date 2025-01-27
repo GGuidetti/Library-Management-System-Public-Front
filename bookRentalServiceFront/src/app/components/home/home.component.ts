ef66eb20b48d83b4c2f0ee03159d0140cedac062
guidera <gustavooguidetti@gmail.com>
2025-01-27 00:40:52 -0300

    **

--- bookRentalServiceFront/src/app/components/home/home.component.ts
+++ bookRentalServiceFront/src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
